abf2effb52f8dfe88fbee9e47e9ccd650efff3b2
TromBoloN <arifvt1212@gmail.com>
2024-07-11 20:42:56 +0800

    Changed routs

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('employees.index');
-});
-
-Route::resource('employees', EmployeeController::class)->except(['show']);
-
-Route::post('/employees', [EmployeeController::class, 'store'])->name('employees.store');
+Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
+Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
+Route::post('/', [EmployeeController::class, 'store'])->name('employees.store');
+Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
